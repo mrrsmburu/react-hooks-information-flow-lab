5fcde91c9a7bb94e8aab308b5a2d735dcdeb8f2e
mrrsmburu <mrrsmburu@gmail.com>
2023-11-29 11:24:33 +0300

    completed assignment

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header( { onDarkModeClick }) {
+    return (
+        <header>
+      <h1>My App</h1>
+      <button onClick={onDarkModeClick}>Toggle Dark Mode</button>
+    </header>
+    )
+}
+
+export default Header;
